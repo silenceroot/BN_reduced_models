3154cc1c7919401dc6004317ba7c60ded7f7e879
ximlel <qq352109085@gmail.com>
2018-04-23 21:41:19 +0800

    new d_x d_y

--- data_out/two-dim/BNR_2_order/TEST/value_plot.py
+++ data_out/two-dim/BNR_2_order/TEST/value_plot.py
@@ -15,8 +15,10 @@ phi_a= np.loadtxt('./PHI_a.csv',delimiter='\t')
 z_a  = np.loadtxt('./Z_a.csv',  delimiter='\t')
 num_x=len(rho[0])
 num_y=len(rho)
-if not (('d_x' in dir()) and ('d_y' in dir())):
-    d_x=0.01;d_y=0.01
+if not ('d_x' in dir()):
+    d_x=0.01
+if not ('d_y' in dir()):
+    d_y=0.01
 X = np.arange(-0.5*num_x, 0.5*num_x, 1)*d_x
 Y = np.arange(-0.5*num_y, 0.5*num_y, 1)*d_y
 X, Y = np.meshgrid(X, Y)
